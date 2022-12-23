ac3a2111527084b5f5685ec99766e6bd5c6331eb
kathirraj <kathiresanvkm@gmail.com>
2022-12-23 15:36:09 +0530

    sales commit

---
+++ vim/sales_management/doctype/sales_invoices/test_sales_invoices.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2022, kathir and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class Testsalesinvoices(FrappeTestCase):
+	pass
