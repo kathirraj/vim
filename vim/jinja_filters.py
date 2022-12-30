9779d96ec995dddefed6feb1963f0290170aba5b
kathirraj <kathiresanvkm@gmail.com>
2022-12-30 12:49:31 +0530

    jinja

---
+++ vim/jinja_filters.py
@@ -0,0 +1,12 @@
+import frappe
+import qrcode
+from PIL import Image
+import base64
+from io import BytesIO
+
+def get_qrcode(input_str):
+    qr = qrcode.make(input_str)
+    temp = BytesIO()
+    qr.save(temp, "PNG")
+    temp.seek(0)
+    b64 = base64.b64encode(temp.read())
