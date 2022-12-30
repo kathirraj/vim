5ad0170d86d101a9ecd31661155827cc0665cdff
kathirraj <kathiresanvkm@gmail.com>
2022-12-30 13:13:57 +0530

    sales remove

--- vim/sales_management/doctype/sales_invoices/sales_invoices.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2022, kathir and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('sales invoices', {
-	// refresh: function(frm) {
-
-	// }
-});
--- vim/sales_management/doctype/sales_invoices/sales_invoices.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2022, kathir and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-class salesinvoices(Document):
-	pass
